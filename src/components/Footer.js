c376e0886672fe4a2a3afc5974e0099d15d2af32
Harsh Mangalam <57381638+harshmangalam@users.noreply.github.com>
2021-04-22 20:10:48 +0530

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="bg-primary-1">
       <div className="max-w-7xl py-5 m-auto">
         <p className="text-lg text-center text-gray-300">
-          Copyright © 2020 playdo All Rights Reserved
+          Copyright © {new Date().getFullYear()} DNA Series
         </p>
       </div>
     </div>
